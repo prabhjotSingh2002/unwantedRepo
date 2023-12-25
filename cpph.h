d1eed0b5b8224b367bc18a248b074a06a67d3300
Sabarishkjm371 <sabarishkjm371@gmail.com>
2023-12-25 11:26:00 +0530

    new task

---
+++ cpph.h
@@ -0,0 +1,19 @@
+#include<cstdio> 
+#include<cstring>
+#include<cmath> 
+#include<unistd.h>
+#include<stdlib.h>
+
+#include<iostream> 
+#include<vector> 
+#include<tuple> 
+#include<algorithm> 
+#include<set> 
+#include<thread> 
+#include<mutex> 
+#include<chrono>
+
+using namespace std; 
+
+typedef unsigned int u32; 
+typedef unsigned char u8;
---
+++ loop.cpp
@@ -0,0 +1,6 @@
+//print a message multiple times
+#include "cpph.h" 
+
+int main(){ 
+
+}
